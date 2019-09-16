24c5e6c96b8fc87fa4f93f32c389ab145fd4bee3
nasroon <nasroones@gmail.com>
2019-09-16 10:36:31 +0700

    2SA03 5th time

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
-const word = "Hello";
+
+import WordCard from './WordCard';
 class App extends Component {
   render() {
     return (
     <div>
-    {
-      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-      }
-      </div>
-      );
-    }
+    <WordCard value="world"/>
+    </div>
+    );
+   }
+   
 }
 export default App;
